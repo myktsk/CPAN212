60e846ca1da478cb0095170ccbb8c4f05d53ca37
myktsk <22167483+myktsk@users.noreply.github.com>
2025-04-06 01:10:37 -0400

    enable deleting a ticket from dashboard

--- labs/lab05/ticket-app/app/TicketPage/[id]/page.jsx
+++ labs/lab05/ticket-app/app/TicketPage/[id]/page.jsx
@@ -1,5 +1,21 @@
 import TicketForm from "@/app/(components)/TicketForm";
 
+const getTicketById = async (id) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch topic");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const TicketPage = ({ params }) => {
   return <TicketForm ticket={{ _id: params.id }} />;
 };
