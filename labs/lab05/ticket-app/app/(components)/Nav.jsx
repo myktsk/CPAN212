dccef9e5601c8f352025a39f1e51b4c4c7e729e2
myktsk <22167483+myktsk@users.noreply.github.com>
2025-04-05 23:33:50 -0400

    global nav

--- labs/lab05/ticket-app/app/(components)/Nav.jsx
+++ labs/lab05/ticket-app/app/(components)/Nav.jsx
@@ -1,5 +1,23 @@
+import Link from "next/link";
+import { faHome, faTicket } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className="flex justify-between p-4 bg-nav">
+      <div className="flex items-center space-x-4">
+        <Link href="/">
+          <FontAwesomeIcon icon={faHome} className="icon" />
+        </Link>
+        <Link href="/TicketPage/new">
+          <FontAwesomeIcon icon={faTicket} className="icon" />
+        </Link>
+      </div>
+      <div>
+        <p className=" text-default-text">example@gmail.com</p>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
